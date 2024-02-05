1f5840e91930f3c590ceddea6eb5953713eeb06b
r.rodrigues <roy71ston@gmail.com>
2024-02-05 10:59:21 +0530

    change

--- src/main/java/com/example/application/views/home/HomePresenter.java
+++ src/main/java/com/example/application/views/home/HomePresenter.java
@@ -2,6 +2,7 @@ package com.example.application.views.home;
 
 
 import com.example.application.dto.*;
+import com.example.application.exception.ErrorResponseException;
 import com.example.application.service.VehicleService;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
@@ -24,46 +25,46 @@ public class HomePresenter {
     private VehicleService vehicleService;
 
     public List<String> getAllTwoWheelerType() {
-        List<String> twoWheelerTypeList = null;
+        List<String> twoWheelerTypeList ;
         try {
-            twoWheelerTypeList = Objects.requireNonNull(vehicleService.getDiscountByTwoWheelerType().getBody()).getResponseData().stream().map(twoWheeler -> twoWheeler.getName()).toList();
+            twoWheelerTypeList = Objects.requireNonNull(vehicleService.getDiscountByTwoWheelerType().getBody()).getResponseData().stream().map(MiscellaneousCost::getName).toList();
         } catch (NullPointerException e) {
             twoWheelerTypeList = new ArrayList<>();
         } catch (Exception e) {
             twoWheelerTypeList = new ArrayList<>();
-            log.info("Error in getAllTwoWheelerType {} ", e);
+            log.error("Error in getAllTwoWheelerType {} ", e);
         }
         return twoWheelerTypeList;
     }
 
     public List<String> getAllVehicleType() {
-        List<String> vehicleTypeList = null;
+        List<String> vehicleTypeList ;
         try {
             vehicleTypeList = Objects.requireNonNull(vehicleService.getVehicleType().getBody()).getResponseData();
         } catch (NullPointerException e) {
             vehicleTypeList = new ArrayList<>();
         } catch (Exception e) {
             vehicleTypeList = new ArrayList<>();
-            log.info("Error in getVehicleType {} ", e);
+            log.error("Error in getVehicleType {} ", e);
         }
         return vehicleTypeList;
     }
 
     public List<Double> getStartAndEndPrice() {
-        List<Double> minAndMaxPrice = null;
+        List<Double> minAndMaxPrice ;
         try {
             minAndMaxPrice = Objects.requireNonNull(vehicleService.getMaxAndMinPrice().getBody()).getResponseData();
         } catch (NullPointerException e) {
             minAndMaxPrice = new ArrayList<>();
         } catch (Exception e) {
             minAndMaxPrice = new ArrayList<>();
-            log.info("Error in getStartAndEndPrice {} ", e);
+            log.error("Error in getStartAndEndPrice {} ", e);
         }
         return minAndMaxPrice;
     }
 
     public Page<VehicleDTO> getAllAvailableVehicles(int pageNumber, int pageSize, SortConfig sortConfig, FilterConfig filterConfig) {
-        Page<VehicleDTO> vehicleList = null;
+        Page<VehicleDTO> vehicleList;
         try {
             vehicleList = Objects.requireNonNull(vehicleService.getAllVehicles(
                     sortConfig.getColumn(),
@@ -76,11 +77,11 @@ public class HomePresenter {
                     pageSize,
                     filterConfig.getFilterValue()
             ).getBody()).getResponseData();
-        } catch (NullPointerException e) {
+        }catch (ErrorResponseException e){
+            vehicleList=new Page<>();
+        }catch (Exception e) {
             vehicleList = new Page<>();
-        } catch (Exception e) {
-            vehicleList = new Page<>();
-            log.info("Error in getAllAvailableVehicles {} ", e);
+            log.error("Error in getAllAvailableVehicles {} ", e);
         }
         return vehicleList;
 
